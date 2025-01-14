e134beb0381d9a35b20bc2b7b85203a6af59cfea
lucas <lcs.carvajal@gmail.com>
2025-01-14 21:34:16 +0100

    some styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,12 +5,16 @@
     function handleSearch() {
         goto('/search');
     }
+
+    function goHome() {
+        goto('/');
+    }
 </script>
 
 <div class="container">
     <div class="header">
         <div class="spacer"></div>
-        <h1 class="page-title">Simple Blog</h1>
+        <a href="/" class="page-title">Simple Blog</a>
         <button class="search-button" aria-label="Search" on:click={handleSearch}>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round">
                 <circle cx="11" cy="11" r="8"></circle>
@@ -45,6 +49,11 @@
     .page-title {
         margin: 0;
         text-align: center;
+        cursor: pointer;
+        text-decoration: none;
+        color: inherit;
+        font-size: 2em;
+        font-weight: bold;
     }
 
     .search-button {
